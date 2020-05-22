b65c681d40664ac6bb5befc7fb92015221a979ff
Melina Alejos <malejos1219@gmail.com>
2020-05-22 09:08:57 -0400

    Generated StudentID and enrolled in courses

--- Student.java
+++ Student.java
@@ -1,3 +1,61 @@
+import java.util.Scanner;
+
+import javax.sound.midi.Soundbank;
+
 public class Student {
-    
+
+    private String firstName, lastName, courses = null, studentID;
+    private int gradeYear, tuitionBalance = 0;
+    private static int id = 1000, costOfCourse = 600;
+
+    // Constructor: prompt user to enter student's name and year
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.println("Enter student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("1 - Freshman \n2 - Sophmore \n3 - Junior \n4 - Senior \nEnter student class level: ");
+        this.gradeYear = in.nextInt();
+
+        setStudentID();
+
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+
+    }
+
+    // Generate an ID
+    private void setStudentID() {
+        // Grade level + ID
+        id++;
+        this.studentID = gradeYear + "" + id;
+    }
+
+    // Enroll in courses
+    public void enroll() {
+        // Get inside a loop, user hits 0
+        do {
+            System.out.print("Enter course to enroll (Q to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")) {
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            } else {
+                System.out.println("BREAK!");
+                break;
+            }
+        } while (1 != 0);
+
+        System.out.println("ENROLLED IN: " + courses);
+        System.out.println("TUITION BALANCE: " + tuitionBalance);
+    }
+
+    // View balance
+
+    // Pay tuition
+
+    // Show status
 }
--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -1,5 +1,10 @@
 public class StudentDatabaseApp {
     public static void main(String[] args) {
 
+        Student stu1 = new Student();
+        stu1.enroll();
+        // Ask how many new users we want to add
+
+        // Create n number of new students
     }
 }
