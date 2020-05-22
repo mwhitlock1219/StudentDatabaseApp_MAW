17c716ac48b196f5bb4fe820edaca9dc0aa1cf53
Melina Alejos <malejos1219@gmail.com>
2020-05-22 09:52:28 -0400

    Created an array of objects/students

--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -1,25 +1,22 @@
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
     public static void main(String[] args) {
 
-        Student stu1 = new Student();
-        stu1.enroll();
-        stu1.payTuition();
-        // System.out.println(stu1.showInfo());
-        // same as below
-        System.out.println(stu1.toString());
-
-        Student stu2 = new Student();
-        stu2.enroll();
-        stu2.payTuition();
-        System.out.println(stu2.toString());
-
-        Student stu3 = new Student();
-        stu3.enroll();
-        stu3.payTuition();
-        System.out.println(stu3.toString());
-
-        // Ask how many new users we want to add
+        // Ask how many new students we want to add
+        System.out.println("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
 
         // Create n number of new students
+        for (int n = 0; n < numOfStudents; n++) {
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+            // System.out.println(stu1.showInfo());
+            // same as below
+            System.out.println(students[n].toString());
+        }
     }
 }
